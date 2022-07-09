39856170633e32a8f8bf67294b9e9c5a6ab047dc
WingDust <995315823@qq.com>
2022-07-09 21:29:33 +0800

    auto-commit

--- play.py
+++ play.py
@@ -32,7 +32,7 @@ IS_WIN32 = 'win32' in str(sys.platform).lower()
 
 
 if __name__ == '__main__':
-    keyboard.hook_key('a', lambda, subprocess_call('mpv "H:/ElectronProject/piano/src/assets/audio/German Concert D 021 083.ogg"')
+    keyboard.hook_key('a', lambda:subprocess_call('mpv "H:/ElectronProject/piano/src/assets/audio/German Concert D 021 083.ogg"')
                       )
     pass
     # app = QtWidgets.QApplication(sys.argv)
